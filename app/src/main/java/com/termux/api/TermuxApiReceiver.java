c549f2b71e9b7809ebe24e8f6463720ddee48ac5
sydneimoon <213307359+sydneimoon@users.noreply.github.com>
2025-05-27 11:42:31 -0400

    🔥 ability to enable/disable wireless ADB

--- app/src/main/java/com/termux/api/TermuxApiReceiver.java
+++ app/src/main/java/com/termux/api/TermuxApiReceiver.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.provider.Settings;
 import android.widget.Toast;
 
+import com.termux.api.apis.AdbWifiAPI;
 import com.termux.api.apis.AudioAPI;
 import com.termux.api.apis.BatteryStatusAPI;
 import com.termux.api.apis.BrightnessAPI;
@@ -84,6 +85,11 @@ public class TermuxApiReceiver extends BroadcastReceiver {
         }
 
         switch (apiMethod) {
+            case "AdbWifiEnable":
+                if (TermuxApiPermissionActivity.checkAndRequestPermissions(context, intent, Manifest.permission.WRITE_SECURE_SETTINGS)) {
+                    AdbWifiAPI.onReceive(this, context, intent);
+                }
+                break;
             case "AudioInfo":
                 AudioAPI.onReceive(this, context, intent);
                 break;
